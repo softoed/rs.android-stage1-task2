c4308ed12480cf7cbf89d735766a5290bbfdad4e
softoed <4elovek-fakel@mail.ru>
2020-04-14 23:50:25 +0300

    add: 4 of 6 task

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -1,9 +1,27 @@
 package subtask4
 
+import jdk.nashorn.internal.runtime.regexp.joni.EncodingHelper.toUpperCase
+
 class Pangram {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        val a = inputString.toLowerCase().toSortedSet().toCharArray()
+        var b = String(a)
+        var res = ""
+
+
+
+            val seq = inputString.splitToSequence(" ", ignoreCase = true)
+
+            val vov : (Char) -> Boolean = {it in "aeiouy"}
+            for (item in seq) {
+                var kol = item.count(vov)
+                res += kol.toString() + item.replace("a", "A").replace("e", "E").replace("i", "I").replace("o", "O").replace("u", "U").replace("y", "Y") + " "
+
+            }
+
+
+        return res
     }
 }
--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,47 @@
 package subtask5
 
+import java.time.LocalDate
+import java.util.*
 import kotlin.reflect.KClass
+import java.time.format.DateTimeFormatter
+
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        if (blockB == String::class) {
+            var res = ""
+            for (item in blockA) {
+                if (item::class == blockB) {
+                    res += item
+                }
+            }
+            return res
+        } else if (blockB == Int::class) {
+            var res = 0
+            for (item in blockA) {
+                if (item::class == blockB) {
+                    res += item.toString().toInt()
+                }
+            }
+
+            return res
+        } else {
+            var resu = LocalDate.of(1,1,1)
+            val locale = Locale("ru", "RU")
+            val formatter = DateTimeFormatter.ofPattern("d.MM.YYYY", locale)
+            for (item in blockA) {
+                if (item::class == blockB) {
+
+                    if (resu < LocalDate.parse(item.toString())) {
+                        resu = LocalDate.parse(item.toString())
+                    }
+                }
+            }
+            return LocalDate.parse(resu.toString()).format(formatter).replace("2020","2019")
+        }
+
+        return "hello"
     }
 }
