ac456814e74057d86b024bab8cc20093928f7217
softoed <4elovek-fakel@mail.ru>
2020-04-14 23:56:52 +0300

    add: 5 of 6 task

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,21 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        var res = 0
+        var a = 0
+        var b = 1
+        for (i in 1..n) {
+            if (a * b == n)
+                return intArrayOf(a, b, 1)
+            else if (a * b > n)
+                return intArrayOf(a, b, 0)
+            else {
+                var c = a
+                a = b
+                b += c
+            }
+        }
+
+        return intArrayOf(1,1,1)
     }
 }
