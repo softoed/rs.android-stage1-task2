25b4a7986802866cf6787da43b3ae58754878345
softoed <4elovek-fakel@mail.ru>
2020-04-16 14:24:32 +0300

    6 of 6

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -6,21 +6,69 @@ class Pangram {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): String {
+        if (inputString.trim().isEmpty()) {
+            return ""
+        }
         val a = inputString.toLowerCase().toSortedSet().toCharArray()
-        var b = String(a)
+        var b = ""
+        for (item in a) {
+            if (item.isLetter()) {
+                b += item
+            }
+        }
+
         var res = ""
+        val mas = arrayOfNulls<String>(10)
+        if (b.length == 26) {
+            val seq = inputString.trim().splitToSequence(" ", ignoreCase = true)
 
-
-
-            val seq = inputString.splitToSequence(" ", ignoreCase = true)
-
-            val vov : (Char) -> Boolean = {it in "aeiouy"}
+            val vov: (Char) -> Boolean = { it in "aeiouy" }
             for (item in seq) {
-                var kol = item.count(vov)
-                res += kol.toString() + item.replace("a", "A").replace("e", "E").replace("i", "I").replace("o", "O").replace("u", "U").replace("y", "Y") + " "
+                val kol = item.count(vov)
+                val newItem = "$kol$item "
+                if (mas[kol].isNullOrEmpty()) {
+                    mas[kol] = newItem
+                } else mas[kol] += newItem
 
             }
+        }
+        else {
+            val seq = inputString.splitToSequence(" ", ignoreCase = true)
 
+            val vov: (Char) -> Boolean = { it in "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz"}
+            for (item in seq) {
+                val kol = item.count(vov)
+                val newItem = "$kol$item "
+                if (mas[kol].isNullOrEmpty()) {
+                    mas[kol] = newItem
+                } else mas[kol] += newItem
+
+            }
+        }
+
+        for (item in mas) {
+            if (item.isNullOrEmpty()) {
+                continue
+            }
+            else {
+                for (cha in item) {
+                    res += when (b.length) {
+                        26 -> {
+                            if (cha in "aeiouy") cha.toUpperCase()
+                            else cha
+                        }
+                        else -> {
+                            if (cha !in "AEIOUYaeiouy") cha.toUpperCase()
+                            else cha
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+        res = res.replace("\n","").replace("0 ", "").trim()
 
         return res
     }
